0380fb7943cd1cd8b8d392358703030aa632ec55
박정현 <tkarnrrns200@gmail.com>
2024-07-22 21:47:29 +0900

    init

---
+++ src/main/java/com/parkjh/stockcrawlingapi/StockCrawlingApiApplication.java
@@ -0,0 +1,13 @@
+package com.parkjh.stockcrawlingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockCrawlingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockCrawlingApiApplication.class, args);
+    }
+
+}
